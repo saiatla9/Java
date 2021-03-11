e777b70de58ed930cf1a6560a5fadf1cc1dbf5f5
saiatla9 <saib9492@gmail.com>
2021-03-11 15:33:06 +0530

    message

---
+++ labexercises/labbook1/Diff.java
@@ -0,0 +1,27 @@
+package com.cg.lb;
+
+public class Diff {
+	static int Square_Diff(int n){ 
+		  
+		int l, k, m; 
+		  
+		    l = (n * (n + 1) * (2 * n + 1)) / 6; 
+		      
+		    k = (n * (n + 1)) / 2; 
+		  
+		    k = k * k;                                    
+		      
+		    m = Math.abs(l - k); 
+		      
+		    return m; 
+		  
+		} 
+		   
+		public static void main(String s[]) 
+		{ 
+		    int n = 21; 
+		    System.out.println(Square_Diff(n));      
+		      
+		} 
+
+}
